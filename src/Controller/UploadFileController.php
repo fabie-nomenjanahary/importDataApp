555f7670abf8a4e905c2fab38386d6556380504d
Fabie Nomenjanahary <fabienomenjanahary@gmail.com>
2024-02-26 00:38:53 +0100

    added upload,import and fetch functionnalities in upload controller

--- src/Controller/UploadFileController.php
+++ src/Controller/UploadFileController.php
@@ -14,13 +14,17 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use App\Form\UploadFileType;
 use App\Entity\Vehicule;
 use App\Entity\Vendeur;
+use App\Form\VehiculeType;
 use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UploadFileController extends AbstractController
 {
-    #[Route('/upload/file', name: 'app_upload_file')]
-    public function index(ManagerRegistry $doctrine,Request $request,VehiculeRepository $vehiculeRepository)
+    #[Route('/', name: 'app_upload_file')]
+    public function index(ValidatorInterface $validator,ManagerRegistry $doctrine,Request $request,VehiculeRepository $vehiculeRepository)
     {
         //Form for uploading the file
         $entityManager=$doctrine->getManager();
@@ -34,22 +38,22 @@ class UploadFileController extends AbstractController
             // Load the uploaded file
             $spreadsheet = IOFactory::load($uploadedFile->getPathname());
 
-            // Remove the heading line
+            // Remove the heading line of the excel file
             $rows = $spreadsheet->getActiveSheet()->removeRow(1);
             //Convert the data into an array
             $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
             foreach ($sheetData as &$row){
                 // Define an array of required column names
-                $requiredColumns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'];
+                $colonnesRequis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI'];
 
                 // Iterate over required columns
-                foreach ($requiredColumns as $column){
+                foreach ($colonnesRequis as $colonne){
                     // If the required column doesn't exist in the current row, create it with null value
-                    if (!isset($row[$column])){
-                        $row[$column] = null;
+                    if (!isset($row[$colonne])){
+                        $row[$colonne] = null;
                     }
                 }
-                // Extract data from the row
+                /****Extract data from the row from the Excel file*****/
                 $compteAffaire = $row['A'];
                 $compteEvent = $row['B'];
                 $dernierEvent = $row['C'];
@@ -84,8 +88,9 @@ class UploadFileController extends AbstractController
                 $numeroDossier = $row['AF'];
                 $intermediaire = $row['AG'];
                 $dateEvent = $this->convertStringToDate('d/m/Y', $row['AH']);
-                $origineEvent = $row['AI'];
-
+                $origineEvent = $row['AI']; 
+                /*----------------------------------------------*/
+                //check if the 'vendeur' already exists or not : save if it doesnt exist
                 $vendeur_existant = $entityManager->getRepository(Vendeur::class)->findOneBy(array('vendeurVN' => $vendeurVN,'vendeurVO' => $vendeurVO,'intermediaire' => $intermediaire));
                 $vendeur=$vendeur_existant;
                 if (!$vendeur_existant)
@@ -96,9 +101,11 @@ class UploadFileController extends AbstractController
                         $vendeur->setVendeurVO($vendeurVO);
                         $vendeur->setIntermediaire($intermediaire);
                         $entityManager->persist($vendeur);
-                        $entityManager->flush();
+                        
                     }
                 }
+
+                //check if the 'adresse' already exists or not : save if it doesnt exist
                 $adresse_existant = $entityManager->getRepository(Adresse::class)->findOneBy(array('codePostal' => $codePostal));
                 $adresse=$adresse_existant;
                 if (!$adresse_existant)
@@ -110,10 +117,11 @@ class UploadFileController extends AbstractController
                         $adresse->setVoie($voie);
                         $adresse->setComplementAdr($complementAdr);
                         $entityManager->persist($adresse);
-                        $entityManager->flush();
+                        
                     }
                 }
 
+                //check if the 'contact' already exists or not : save if it doesnt exist
                 $contact_existant = $entityManager->getRepository(Contact::class)->findOneBy(array('email' => $email,'telDomicile' => $telDomicile,'telPortable' => $telPortable));
                 $contact=$contact_existant;
                 if (!$contact_existant)
@@ -125,10 +133,11 @@ class UploadFileController extends AbstractController
                         $contact->setTelPortable($telPortable);
                         $contact->setTelJob($telJob);
                         $entityManager->persist($contact);
-                        $entityManager->flush();
+                        
                     }
                 }
 
+                //check if the 'proprietaire' already exists or not : save if it doesnt exist
                 $proprietaire_existant = $entityManager->getRepository(Proprietaire::class)->findOneBy(array('surnom' => $surnom,'nom'=>$nom));
                 $proprietaire=$proprietaire_existant;
                 if (!$proprietaire_existant)
@@ -142,10 +151,11 @@ class UploadFileController extends AbstractController
                         $proprietaire->setContact($contact);
                         $proprietaire->setCivilite($civilite);
                         $entityManager->persist($proprietaire);
-                        $entityManager->flush();
+                        
                     }
                 }
 
+                //check if the 'compte' already exists or not : save if it doesnt exist
                 $compte_existant = $entityManager->getRepository(Compte::class)->findOneBy(array('compteAffaire' => $compteAffaire,'compteEvent'=>$compteEvent,'dernierEvent'=>$dernierEvent));
                 $compte=$compte_existant;
                 if (!$compte_existant)
@@ -156,10 +166,27 @@ class UploadFileController extends AbstractController
                         $compte->setCompteEvent($compteEvent);
                         $compte->setDernierEvent($dernierEvent);
                         $entityManager->persist($compte);
-                        $entityManager->flush();
+                        
                     }
                 }
 
+                //check if the 'evenement' already exists or not : save if it doesnt exist
+                $evenement_existant = $entityManager->getRepository(Evenement::class)->findOneBy(array('origineEvent' => $origineEvent,'dateEvent' => $dateEvent,'dateDernierEvent' => $dateDernierEvent,'commentaire' => $commentaire));
+                $evenement=$evenement_existant;
+                if (!$evenement_existant)
+                {
+                    if ($origineEvent!=null || $commentaire!=null || $dateEvent!=null || $dateDernierEvent!=null) {
+                        $evenement = new Evenement();
+                        $evenement->setOrigineEvent($origineEvent);
+                        $evenement->setCommentaire($commentaire);
+                        $evenement->setDateEvent($dateEvent);
+                        $evenement->setDateDernierEvent($dateDernierEvent);
+                        $entityManager->persist($evenement);
+                        
+                    }
+                }
+
+                //check if the 'vehicule' already exists or not : save if it doesnt exist
                 $vehicule_existant = $entityManager->getRepository(Vehicule::class)->findOneBy(array('VIN' => $VIN,'matricule'=>$matricule));
                 $vehicule=$vehicule_existant;
                 if (!$vehicule_existant)
@@ -182,53 +209,37 @@ class UploadFileController extends AbstractController
                         $vehicule->setProprietaire($proprietaire);
                         $vehicule->setCompte($compte);
                         $vehicule->setVendeur($vendeur);
+                        $vehicule->setEvenement($evenement);
                         $entityManager->persist($vehicule);
-                        $entityManager->flush();
                     }
                 }
-
-                $evenement_existant = $entityManager->getRepository(Evenement::class)->findOneBy(array('origineEvent' => $origineEvent,'vehicule' => $vehicule));
-                $evenement=$evenement_existant;
-                if (!$evenement_existant)
-                {
-                    if ($origineEvent!=null || $commentaire!=null || $dateEvent!=null || $dateDernierEvent!=null) {
-                        $evenement = new Evenement();
-                        $evenement->setOrigineEvent($origineEvent);
-                        $evenement->setCommentaire($commentaire);
-                        $evenement->setDateEvent($dateEvent);
-                        $evenement->setDateDernierEvent($dateDernierEvent);
-                        $evenement->setVehicule($vehicule);
-                        $entityManager->persist($evenement);
-                        $entityManager->flush();
-                    }
-                }
+                $entityManager->flush();
             }
+            $this->addFlash('success','Données importées et enregistrées avec succès');
 
         }
-        // $data = $entityManager->getRepository(Vendeur::class)->findOneBy(array('vendeurVN' => $vendeurVN,'vendeurVO' => $vendeurVO,'intermediaire' => $intermediaire));
-        // $data=$vehiculeRepository->findAllWithRelatedData();
-        $vehicules = $entityManager->getRepository(Vehicule::class)
-        ->createQueryBuilder('v')
-        ->leftJoin('v.compte', 'c')
-        ->leftJoin('v.proprietaire', 'p')
-        ->leftJoin('p.adresse', 'a')
-        ->leftJoin('p.contact', 'ct')
-        ->leftJoin('v.vendeur', 'vd')
-        ->leftJoin('v.evenements', 'e')
-        ->getQuery()
-        ->getResult();
-        dd($vehicules);
+        //get error messages from the form and send it to view
+        $errors = $validator->validate($form);
+        $errorMessages = [];
+        foreach ($errors as $error) {
+            $errorMessages[] = $error->getMessage();
+        }
+
+        //get all the saved 'vehicules' with their related data : compte,evenement,proprietaire,vendeur...
+        $vehicules = $entityManager->getRepository(Vehicule::class)->findAllVehiculesWithAllRelatedData();
+
         return $this->render('upload_file/upload.html.twig', [
             'form' => $form->createView(),
-            'data'=>[]
+            'data'=>$vehicules,
+            'errors'=>$errorMessages
         ]);
     }
     public function convertStringToDate($format,$dateString){
         $dateConverted=null;
         if ($dateString!=null) {
-            # code...
             $dateConverted= \DateTime::createFromFormat($format, $dateString);
             if (!($dateConverted instanceof \DateTime)) {
+                //if not a datetime object
                 $dateConverted=null;
             }
         }
