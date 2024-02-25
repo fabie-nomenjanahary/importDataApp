dc4fc6b836254fe64f3f13fe30518296687a9542
Fabie Nomenjanahary <fabienomenjanahary@gmail.com>
2024-02-26 00:43:16 +0100

    created a controller for vehicule and its related data and added crud operations

---
+++ src/Controller/VehiculeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicule;
+use App\Form\VehiculeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/vehicule')]
+class VehiculeController extends AbstractController
+{
+    #[Route('/', name: 'app_vehicule_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_upload_file', [], Response::HTTP_SEE_OTHER);
+
+    }
+
+    #[Route('/new', name: 'app_vehicule_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $vehicule = new Vehicule();
+        $form = $this->createForm(VehiculeType::class, $vehicule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //save all related data if only it is submitted and valid
+            $entityManager->persist($vehicule);
+            $entityManager->flush();
+            $this->addFlash('success','Données enregistrées avec succès');
+            return $this->redirectToRoute('app_upload_file', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('vehicule/new.html.twig', [
+            'vehicule' => $vehicule,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vehicule_show', methods: ['GET'])]
+    public function show(Vehicule $vehicule): Response
+    {
+        //show details of the remaining data
+        return $this->render('vehicule/show.html.twig', [
+            'vehicule' => $vehicule,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_vehicule_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(VehiculeType::class, $vehicule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //save changes
+            $entityManager->flush();
+            $this->addFlash('success','Données modifiées avec succès');
+            return $this->redirectToRoute('app_upload_file', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('vehicule/edit.html.twig', [
+            'vehicule' => $vehicule,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vehicule_delete', methods: ['POST'])]
+    public function delete(Request $request, Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$vehicule->getId(), $request->request->get('_token'))) {
+            //only remove all related data if a token is found
+            $entityManager->remove($vehicule);
+            $entityManager->flush();
+            $this->addFlash('success','Données supprimées avec succès');
+
+        }
+
+        return $this->redirectToRoute('app_upload_file', [], Response::HTTP_SEE_OTHER);
+    }
+}
